d7d3a264079bfe8c2a6226c78a6f0822b258adf0
aszczepanski <aszczepanski92@gmail.com>
2014-01-08 15:36:05 +0100

    basic request and reply classes

---
+++ src/http/reply.cpp
@@ -0,0 +1,1 @@
+#include "http/reply.h"
--- src/http/request.h
+++ src/http/request.h
@@ -1,4 +1,21 @@
 #ifndef SRC_HTTP_REQUEST_H_
 #define SRC_HTTP_REQUEST_H_
 
+#include <string>
+#include <vector>
+#include "http/header.h"
+
+namespace http {
+
+  class Request {
+   public:
+    std::string method;
+    std::string uri;
+    int http_version_major;
+    int http_version_minor;
+    std::vector<Header> headers;
+  };
+
+}  // namespace http
+
 #endif  // SRC_HTTP_REQUEST_H_
