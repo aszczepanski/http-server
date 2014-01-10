2154dfde00359efb6e7f27301126ae8e3d703480
Adam Szczepański <aszczepanski92@gmail.com>
2014-01-10 20:11:25 +0100

    thread interface and mutex

---
+++ src/thread/thread_interface.cpp
@@ -0,0 +1,24 @@
+#include "thread/thread_interface.h"
+
+#include <pthread.h>
+
+using thread::ThreadInterface;
+
+ThreadInterface::ThreadInterface() {
+}
+
+ThreadInterface::~ThreadInterface() {
+}
+
+void ThreadInterface::Run() {
+  pthread_create(&thread, NULL, StaticRoutine, (void*)this);
+}
+
+void ThreadInterface::Wait() {
+  pthread_join(thread, NULL);
+}
+
+void* ThreadInterface::StaticRoutine(void* arg) {
+  ThreadInterface *ithread = (ThreadInterface*)arg;
+  return ithread->StartRoutine();
+}
--- src/thread/thread_interface.h
+++ src/thread/thread_interface.h
@@ -1,7 +1,24 @@
 #ifndef SRC_THREAD_THREAD_INTERFACE_H_
 #define SRC_THREAD_THREAD_INTERFACE_H_
 
+#include <pthread.h>
+
+namespace thread {
+
 class ThreadInterface {
+ public:
+  ThreadInterface();
+  virtual ~ThreadInterface();
+
+  void Run();
+  void Wait();
+ protected:
+  virtual void* StartRoutine() = 0;
+ private:
+  pthread_t thread;
+  static void* StaticRoutine(void* arg);
 };
+
+}  // namespace thread
 
 #endif  // SRC_THREAD_THREAD_INTERFACE_H_
