cf103f1ca387dd616fcd9e798d89c9e303bdde81
Tomasz Pewiński <pewniak747@gmail.com>
2014-02-12 17:05:44 +0100

    make QT work with cmake

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,3 +1,11 @@
-int main() {
-  return 0;
+#include "uploaderwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    UploaderWindow w;
+    w.show();
+
+    return a.exec();
 }
